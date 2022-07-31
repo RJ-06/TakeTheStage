13b688fc498b0b242d1237a10474362d6ebe66d9
RJ-06 <54755468+RJ-06@users.noreply.github.com>
2022-07-31 11:44:35 +0530

    sword

--- Theater/Assets/Scripts/PlayerOne.cs
+++ Theater/Assets/Scripts/PlayerOne.cs
@@ -4,18 +4,25 @@ using UnityEngine;
 
 public class PlayerOne : MonoBehaviour
 {
-
+    //movement
     [SerializeField] float moveSpeed;
     [SerializeField] float jumpForce;
     [SerializeField] float jumpChange;
     [SerializeField] Rigidbody2D rb;
+    private bool facingRight = true;
 
+    //groundcheck
     private bool isGrounded;
     [SerializeField] Transform groundCheck;
     [SerializeField] float checkRadius;
     [SerializeField] LayerMask whatIsGround;
 
-    private bool facingRight = true;
+    //attack and combat
+    public GameObject sword;
+    public int health = 100;
+    [SerializeField] int damage;
+    [SerializeField] Transform swordPos;
+    private float attackWaitTime = 1;
 
     void Start()
     {
@@ -32,6 +39,11 @@ public class PlayerOne : MonoBehaviour
         {
             rb.velocity = new Vector2(rb.velocity.x, rb.velocity.y * jumpChange);
         }
+
+        if (Input.GetKeyDown(KeyCode.V) && attackWaitTime < 0) {
+            attack();
+        }
+        attackWaitTime -= Time.deltaTime;
     }
 
 
@@ -43,10 +55,11 @@ public class PlayerOne : MonoBehaviour
         float moveInput = Input.GetAxisRaw("p1Horizontal");
         rb.velocity = new Vector2(moveInput * moveSpeed, rb.velocity.y);
 
-        if ((facingRight == false && moveInput < 0) || (facingRight == true && moveInput > 0))
+        if ((facingRight == false && moveInput > 0) || (facingRight == true && moveInput < 0))
         {
             flip();
         }
+
     }
 
 
@@ -55,5 +68,18 @@ public class PlayerOne : MonoBehaviour
         Vector3 Scaler = transform.localScale;
         Scaler.x *= -1;
         transform.localScale = Scaler;
+    }
+
+    void attack() {
+        GameObject tempSword = Instantiate(sword,swordPos);
+        Destroy(tempSword, .5f);
+        attackWaitTime = 1f;
+    }
+
+    private void OnCollisionEnter2D(Collision2D col)
+    {
+        if (col.gameObject.CompareTag("p2Sword")) {
+            health -= damage;   
+        }
     }
 }
