8ccd42ae40a2d25d2c59f5d3f81fa637374ef426
RJ-06 <54755468+RJ-06@users.noreply.github.com>
2022-11-30 21:32:47 -0800

    music thingy that won't really be used for anything in game but is cool

--- Theater/Assets/Scripts/Modulation.cs
+++ Theater/Assets/Scripts/Modulation.cs
@@ -1,18 +1,44 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Modulation : MonoBehaviour
 {
     // Start is called before the first frame update
+    public AudioSource a;
+    public Slider s;
+    public bool locked = false;
+
+    [SerializeField] float minPitch;
+    [SerializeField] float maxPitch;
+
+    [SerializeField] char key;
+
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        a.pitch = .01f * s.value * (maxPitch - minPitch) + minPitch;
+
+
+
+        if (Input.GetMouseButtonDown(0) && locked)
+        {
+            a.Play();
+        }
+
+
     }
+
+    public void lockButton()
+   {
+        locked = !locked;
+   }
+
+    
 }
